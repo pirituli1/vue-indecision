627c4e6cc62a925c6c5984d4933f3d2c08cfa51f
Carlos Enrique Bautista Velasco <carlosebv_cfeit_2023@outlook.com>
2024-07-11 14:59:20 -0600

    Se agregó todo hasta 57.Comunicacion en componentes minuto:7:12

---
+++ src/03-indecision-app/src/components/chat/chatMessage.vue
@@ -0,0 +1,33 @@
+<template> 
+
+ <!-- *Chat message BOX -->
+ <div class="flex-1 overflow-y-auto p-4">
+        <div class="flex flex-col space-y-2">
+
+         <chatBubble 
+         v-for="message in messages"
+         :key="message.id" 
+         v-bind="message"
+         />
+         <!-- :its-mine="message.itsMine"
+         :message="message.message"
+         :image="message.image" -->
+         
+
+
+        </div>
+      </div>
+      <!-- *Chat message box -->
+
+</template>
+
+<script setup lang="ts">  
+  import chatBubble from '@/components/chat/chatBubble.vue';
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
+  interface Props {
+    messages: ChatMessage[];
+  }
+
+
+  defineProps<Props>();
+</script>
---
+++ src/03-indecision-app/src/views/indecisionView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+      <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+        <span>Solecito</span>
+      </div>
+
+     <!-- Chat messages -->
+      <chatMessage :messages="messages"/>
+
+      <messageBox/>
+      
+      
+    </div>
+  </template>
+
+<script setup lang="ts">
+  import chatMessage from '@/components/chat/chatMessage.vue';
+  import messageBox from '@/components/chat/messageBox.vue';
+import type{ ChatMessage } from '@/interfaces/chat-message.interface';
+  import { ref } from 'vue';
+
+  const messages = ref<ChatMessage[]>([
+    {
+    id: new Date().getTime(),
+    message: "Quieres ir a tomar café?",
+    itsMine: true
+    },
+    {
+    id: new Date().getTime()+1,
+    message: "Yes",
+    itsMine: false,
+    image: "https://yesno.wtf/assets/yes/9-6403270cf95723ae4664274db51f1fd4.gif",
+    },
+    {
+    id: new Date().getTime()+1,
+    message: "Que bueno, yo también quiero",
+    itsMine: true,
+    image: "https://yesno.wtf/assets/yes/9-6403270cf95723ae4664274db51f1fd4.gif",
+    },
+]);
+
+
+
+</script>
